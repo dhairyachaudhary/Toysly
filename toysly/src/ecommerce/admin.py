699a171903ade31b039689afa2bb323e9e62c2ab
Kirthana <kirthana19053@iiitd.ac.in>
2021-11-05 15:42:56 +0530

    Added models to admin page

--- toysly/src/ecommerce/admin.py
+++ toysly/src/ecommerce/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Buyer)
+admin.site.register(Seller)
+admin.site.register(Product)
+admin.site.register(ProductImages)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Shipping_Details)
+admin.site.register(Cart)
