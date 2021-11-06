9b3637f8ab770878093ad492343df81526620f01
KaranAbrol <karan.abrol1@gmail.com>
2021-11-06 17:43:45 +0530

    Added signup, login, logout pages/buttons.

--- toysly/src/accounts/views.py
+++ toysly/src/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
@@ -7,9 +8,25 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            #log the user in
+            user = form.save()
+            login(request,user)
             return redirect('store:store')
     else:
         form = UserCreationForm()
     return render(request,"accounts/signup.html",{'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('store:store')
+    else:
+        form = AuthenticationForm()
+    return render(request,"accounts/login.html",{'form':form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('store:store')
