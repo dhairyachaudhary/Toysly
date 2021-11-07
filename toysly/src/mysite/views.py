e8f1eb896888f87421f0a63aab106ca2a0caf707
Kirthana <kirthana19053@iiitd.ac.in>
2021-11-07 10:12:19 +0530

    Render home and about pages

--- toysly/src/mysite/views.py
+++ toysly/src/mysite/views.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('homepage')
+    return render(request,'home.html')
 
 
 def about(request):
-    return HttpResponse('about')
+    return render(request,'about.html')
 
