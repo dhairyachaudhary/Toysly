59509183ab4401023d4d4db514341116d471b961
Dhairya <dhairya19035@iiitd.ac.in>
2021-11-08 00:01:10 -0800

    adding product images

--- toysly/src/mysite/views.py
+++ toysly/src/mysite/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('homepage')
+    return render(request,'index.html')
 
 
 def about(request):
