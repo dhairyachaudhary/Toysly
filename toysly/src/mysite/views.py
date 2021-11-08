866cc66a562099a6215105fd7e4b862c6882e02e
Dhairya <dhairya19035@iiitd.ac.in>
2021-11-08 05:08:38 -0800

    updated home page

--- toysly/src/mysite/views.py
+++ toysly/src/mysite/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'base.html')
+    return render(request,'index.html')
 
 
 def about(request):
