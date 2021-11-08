1bb2ae4e1f3338537861dc375eb416b710320446
KaranAbrol <karan.abrol1@gmail.com>
2021-11-08 15:17:52 +0530

    Something

--- toysly/src/ecommerce/forms.py
+++ toysly/src/ecommerce/forms.py
@@ -5,7 +5,7 @@ from . import models
 class ProductForm(forms.ModelForm):
 	class Meta:
 		model = models.Product
-		fields = ['product_name','product_brand','product_category','product_price','product_description','product_quantity_available','product_delivery_time']
+		fields = ['product_name','product_brand','product_category','product_price','product_description','product_delivery_time']
 
 class ProductImagesForm(forms.ModelForm):
 	class Meta:
--- toysly/src/mysite/views.py
+++ toysly/src/mysite/views.py
@@ -2,11 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-<<<<<<< HEAD
-    return render(request,'index.html')
-=======
-    return render(request,'home.html')
->>>>>>> a0ae616d5a4439f93b63511eee8373bff325da31
+    return render(request,'base.html')
 
 
 def about(request):
