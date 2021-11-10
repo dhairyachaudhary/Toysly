55aade6bc84e198c26a0d11b65b94f596802fe77
Dhairya <dhairya19035@iiitd.ac.in>
2021-11-10 08:26:13 -0800

    added virtual keyboard

--- toysly/src/accounts/views.py
+++ toysly/src/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
